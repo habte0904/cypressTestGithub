2a8ee79658a4c802f29ca8eb94abcf99876e75d2
Habtamu Petros <89646714+habte0904@users.noreply.github.com>
2022-09-01 04:45:30 +0300

    Second cypress code with github

--- cypress/e2e/testCase1.cy.js
+++ cypress/e2e/testCase1.cy.js
@@ -3,7 +3,7 @@ describe("First Cypress project",function(){
   
   it("visit url",()=>{
     cy.visit("https://www.google.com")
-    cy.get('.gLFyf').type("hello world")
+    cy.get('.gLFyf').type("hello world ethio")
     
   })
 
